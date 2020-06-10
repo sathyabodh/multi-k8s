ada8b568bafe8e1c6807be0664aa6f6aa8e46dc7
sathyabodh <sathyabodh.mudhol@sap.com>
2020-06-10 22:31:04 +0530

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t sathyabodh/multi-client:latest -t sathyabodh/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sathyabodh/multi-server:latest -t sathyabodh/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sathyabodh/multi-worker:latest  -t sathyabodh/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push sathyabodh/multi-client:latest
+docker push sathyabodh/multi-server:latest
+docker push sathyabodh/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push sathyabodh/multi-client:$SHA
+docker push sathyabodh/multi-server:$SHA
+docker push sathyabodh/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+
+kubectl set image deployment/server-deployment server=sathyabodh/multi-server:$SHA
+kubectl set image deployment/client-deployment client=sathyabodh/multi-client:$SHA
+kubectl set image deployment/worker-deployment worker=sathyabodh/multi-worker:$SHA
+
